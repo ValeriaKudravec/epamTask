4e7939552e93f7d20007dc30c5832727b272d5bd
ValeriaKudravec <valeriakudravec@gmail.com>
2019-09-24 11:04:09 +0300

    Add validation and input

--- task1/src/task1/Main.java
+++ task1/src/task1/Main.java
@@ -1,18 +1,38 @@
 package task1;
 
 import task1.actionClass.Sorts;
+import task1.validationClass.Validator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> array = new ArrayList<>();
-        for(int i = 5; i >= 0; i--){
-            array.add(1.0*i);
+        try(Scanner scanner = new Scanner(new File("input/inputFile.txt"))){
+            Validator validator = new Validator();
+            List<List<Double> > array = new ArrayList<>();
+            int counter = 0;
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if(validator.validate(line)){
+                    array.add(new ArrayList<>());
+                    for (String number:line.split(" +")) {
+                        array.get(array.size()-1).add(Double.parseDouble(number));
+                    }
+                    System.out.println(line);
+                    Sorts sorts = new Sorts(array.get(counter));
+                    sorts.insertionSort();
+                    sorts.gerArray().forEach(item->System.out.print(item + " "));
+                }
+            }
         }
-        Sorts sorts = new Sorts(array);
-        sorts.insertionSort();
-        sorts.gerArray().forEach(item->System.out.print(item + " "));
+        catch (FileNotFoundException e) {
+            System.out.println("file is not found");
+        }
+
+
     }
 }
